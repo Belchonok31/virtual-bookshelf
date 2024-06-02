40a944dbde2c4651c86479c918a6cee6b5f2f1d5
Belchonok31 <egor.belosludtsev@mail.ru>
2024-06-03 01:51:57 +0300

    fix 2 form bookOriginal

--- frontend/src/pages/bookOriginalList/BookOriginalPage.jsx
+++ frontend/src/pages/bookOriginalList/BookOriginalPage.jsx
@@ -62,7 +62,7 @@ const BookOriginalPage = () => {
         setBookForm({ ...bookForm, [e.target.name]: e.target.value });
     };
     const handleBookOriginalChange = (e) => {
-        setBookOriginalForm({ ...bookForm, [e.target.name]: e.target.value });
+        setBookOriginalForm({ ...bookOriginalForm, [e.target.name]: e.target.value });
     };
 
     const handleShow = (BookOriginal = null) => {
